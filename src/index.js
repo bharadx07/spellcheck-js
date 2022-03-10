a9c19ebdca4dc70c798f68e5d5eb540703935d7c
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2022-03-09 20:11:49 -0800

    feat: start result class and fn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 const dictionary = require("./dictionary");
 
-console.log(dictionary.check("fdsa"))
+class Result {
+  isAllEnglish = true;
+  englishWords = [];
+  notEnglishWords = [];
+
+  constructor(englishString) {
+    this.englishString = englishString;
+  }
+
+  evaluate() {
+    const eachWord = this.englishString.split(" ");
+    eachWord.forEach((word) => {
+      const isEnglish = dictionary.check(word);
+
+      if (isEnglish) {
+        this.englishWords.push(word);
+      } else {
+        this.notEnglishWords.push(word);
+        this.isAllEnglish = false;
+      }
+    });
+
+    return this;
+  }
+
+  formatted() {
+    return {
+      isAllEnglish: this.isAllEnglish,
+      englishWords: this.englishWords,
+      notEnglishWords: this.notEnglishWords,
+    };
+  }
+}
+
+const allEnglishWords = (englishString) => {
+  return new Result(englishString).evaluate().formatted();
+};
+
+console.log(allEnglishWords("hey", {}).isAllEnglish);
